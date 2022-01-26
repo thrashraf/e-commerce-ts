206d2d1f34abd2e4ec3bba03e28c4e51a996e76c
Ashraf <zulashrafvns@gmail.com>
2022-01-26 16:57:55 +0800

    add detail product

---
+++ src/pages/Product.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import products from '../products';
+import star from '../assets/star.png'
+
+type Props = {};
+
+const Product = (props: Props) => {
+
+    const { id } = useParams()
+    
+    const product = products.find((prod) => prod._id === id )
+
+    return product ? 
+    (<div className='grid grid-cols-2 max-w-7xl m-auto px-10 gap-20 mt-10'>
+        <section>
+            <img src={product.image} alt='product' className='rounded-lg'/>
+        </section>
+
+        <section>
+            <h1 className='text-2xl'>{product.name}</h1>
+
+            <section className='flex justify-between w-[200px] mt-2 text-gray-500 text-md'>
+                <aside className='flex items-center'>
+                    <p className='text-gray-500 mr-1'>{product.rating}</p>
+                    <img src={star} alt='rating' className='w-4 h-4 '/>
+                </aside>
+
+                <aside>
+                    <p>Ratings {product.numReviews}</p>
+                </aside>
+
+                <aside>
+                    <p>{product.numReviews} Sold</p>
+                </aside>
+            </section>
+
+            <h3 className='text-3xl font-medium my-10'>${product.price}</h3>
+
+
+            <section>
+                <h3 className='text-lg'>Product Specifications</h3>
+
+                <section className='w-[200px] flex justify-between text-gray-500 mt-5 text-sm'>
+                    <aside>
+                        <p>Category</p>
+                        <p>Stock</p>
+                        <p>Brand</p>
+                    </aside>
+
+                    <aside>
+                        <p>{product.category}</p>
+                        <p>{product.countInStock}</p>
+                        <p>{product.brand}</p>
+                    </aside>
+                </section>
+            </section>
+
+            <section className='mt-10 max-w-md '>
+                <h3 className='text-lg'>Product Description</h3>
+                <p className='text-gray-500 text-sm mt-5'>{product.description}</p>
+            </section>
+
+            
+
+
+        </section>
+    </div>)
+
+    : null
+};
+
+export default Product;
