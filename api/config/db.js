876d18e5c8dc8f45a7e0833aadf20138b3815518
Ashraf <zulashrafvns@gmail.com>
2022-01-28 20:58:31 +0800

    create db config

---
+++ api/config/db.js
@@ -0,0 +1,24 @@
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const pool = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+})
+
+const sql = 'SELECT * FROM products';
+
+pool.execute(sql, (err, result) => {
+
+    if (err) throw err;
+
+    console.log(result);
+
+});
+
+export default pool.promise()
+
