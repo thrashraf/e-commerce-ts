32f43313ddd180ed133e82685fd60e0975318bcb
Ashraf <zulashrafvns@gmail.com>
2022-01-29 20:04:13 +0800

    add product detail reducer & actions

--- api/controller/productsController.js
+++ api/controller/productsController.js
@@ -1,17 +1,29 @@
 import products from "../model/products.js"
 
 export const getAllProducts = async (req, res, next) => {
-    const [allProducts] = await products.getAllProducts()
-    console.log(allProducts);
-    res.status(200).json({allProducts});
-}
+
+    try {
+        const [allProducts] = await products.getAllProducts()
+        //console.log(allProducts);
+        res.status(200).json({allProducts});
+    } catch (error) {
+        next(error)
+    }
+
+};
 
 
 export const getProductsById = async (req, res, next) => {
 
-    const id = req.params.id
+    try {
+        const id = req.params.id
+    
+        const [productById] = await products.getProductById(id)
+        console.log(productById);
+        res.status(200).json({productById});
 
-    const [productById] = await products.getProductById(id)
-    console.log(productById);
-    res.status(200).json({productById});
+    } catch (error) {
+        next(error)
+    }
+
 }
---
+++ frontend/src/reducers/productDetailReducers.ts
@@ -0,0 +1,28 @@
+import { productDetail } from "../constant/productDetailConstant"
+
+export const productDetailReducers = (state = {productById: []}, action:any) => {
+
+    switch (action.type) {
+        case productDetail.REQUEST_PRODUCT_DETAIL:
+            return { 
+                loading: true, 
+                productById: []
+            }
+
+        case productDetail.SUCCESS_REQUEST_PRODUCT:
+            return { 
+                loading: false, 
+                productById: action.payload
+            }
+
+
+        case productDetail.FAIL_REQUEST_PRODUCT:
+            return { 
+                loading: false, 
+                error: action.payload
+            }
+            
+        default:
+            return state;
+    }
+}
