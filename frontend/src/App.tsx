ebf5f60b93affb9c8b0c72b73c7c650485e88cf6
Ashraf <zulashrafvns@gmail.com>
2022-01-30 12:09:49 +0800

    add login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import './styles/index.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import {Navbar}  from './components/Navbar'
 import { Home } from './pages/Home';
+import { Login } from './pages/Login';
 import Product from './pages/Product';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/product/:id' element={<Product />} />
+          <Route path='/login' element={<Login />} />
         </Routes>
     </BrowserRouter>
     </div>
