98240f597f8e6e6264211289fab2b30ff76718da
Ashraf <zulashrafvns@gmail.com>
2022-02-02 19:27:09 +0800

    working on login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,7 @@ import {Navbar}  from './components/Navbar'
 import { Home } from './pages/Home';
 import { Login } from './pages/Login';
 import { SignUp } from './pages/SignUp';
+import { Profile } from './pages/Profile';
 import Product from './pages/Product';
 
 
@@ -37,6 +38,7 @@ function App() {
           <Route path='/product/:id' element={<Product />} />
           <Route path='/login' element={<Login />} />
           <Route path='/signup' element={<SignUp />} />
+          <Route path='/profile/:id' element={<Profile />} />
         </Routes>
     </BrowserRouter>
     </div>
---
+++ frontend/src/pages/Profile.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+type Props = {};
+
+export const Profile = (props: Props) => {
+  return <div>
+
+      <h1>Profile</h1>
+  </div>;
+};
