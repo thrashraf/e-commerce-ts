da1e97c40dad058b254a99d98356a8a2d2fb63a0
Ashraf <zulashrafvns@gmail.com>
2022-02-02 12:11:21 +0800

    add refresh action

---
+++ api/controller/refreshToken.js
@@ -0,0 +1,42 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import user from '../model/user.js'
+
+dotenv.config()
+
+export const refreshToken = async (req, res, next) => {
+
+    try {
+        
+        const token = req.cookies.token
+        console.log(req.cookies.token);
+        const id = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+
+        if (token && id) {
+            console.log('first');
+            const [verifyUser] = await user.user(id);
+            const userInfo = verifyUser[0];
+
+            
+            res.status(200).json(
+                {
+                    isAuth: true,
+                    firstName: userInfo.firstName,
+                    lastName: userInfo.lastName,
+                    role: userInfo.role,
+                    isVerified: userInfo.isVerified,
+                    redirect: '/'
+                }
+            )
+
+        } else {
+            throw new Error('invalid user');
+        }
+
+    } catch (error) {
+        res.status(401).json({message: 'invalid password lol'})
+        res.json({error})
+    }
+
+    
+}
--- frontend/src/actions/loginActions.ts
+++ frontend/src/actions/loginActions.ts
@@ -5,7 +5,6 @@ import { loginConstant } from "../constant/loginConstant";
 export const loginUser = (email: string, password: string) => async (dispatch: any) => {
 
     try {
-
         //console.log(email, password);
         dispatch({type: loginConstant.REQUEST_LOGIN})
         const user = await axios.post(`http://localhost:5000/api/login`, {email, password}, {withCredentials: true})
