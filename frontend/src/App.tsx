6326dab2b9b23076aca06a805ac4c5a8382e439b
Ashraf <zulashrafvns@gmail.com>
2022-02-05 21:53:50 +0800

    add modal and backdrop

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles/index.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useDispatch, useSelector, RootStateOrAny } from 'react-redux';
@@ -9,12 +9,16 @@ import { Login } from './pages/Login';
 import { SignUp } from './pages/SignUp';
 import { Profile } from './pages/Profile';
 import Product from './pages/Product';
+import { Modal } from './components/Modal'
+import Module from 'module';
 
 
 function App() {
   
   const dispatch = useDispatch()
   const userDetail = useSelector((state: RootStateOrAny) => state.loginReducer);
+
+  const [modal, setModal] = useState<boolean>(false);
 
   const { user } = userDetail;
 
@@ -31,6 +35,11 @@ function App() {
   return (
 
     <div className="h-full w-full font-custom">
+      
+      {/* Backdrop */}
+      <div className={`w-full h-full bg-[#00000070] z-10 absolute m-0 p-0 ${modal ? 'block' : 'hidden'} `}  />
+      
+      
       <BrowserRouter>
       <Navbar />
         <Routes>
