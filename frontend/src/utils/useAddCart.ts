158b616050aec2329f9b1b67538d000f677b5739
Ashraf <zulashrafvns@gmail.com>
2022-02-20 16:51:56 +0800

    add quantity

---
+++ frontend/src/utils/useAddCart.ts
@@ -0,0 +1,8 @@
+import { useDispatch } from 'react-redux';
+import { addToCart } from '../services/cart/cartAction';
+
+export const useAddCart = (cart: any[], productById: any) => {
+
+  const dispatch = useDispatch();
+  
+}
